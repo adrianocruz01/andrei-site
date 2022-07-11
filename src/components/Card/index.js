5b1b5597372d577e1a7344d81566cc5e75760fa6
Bruno Gonçalves Costa <br9uno@gmail.com>
2022-07-11 15:11:04 -0300

    acerto de layout e responsividade

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,16 +3,7 @@ import "./style.css";
 export default function Card({ title, description, url, cover }) {
 	return (
 		<div className='card'>
-			<img className="img-video" src={cover} alt='card cover' />
-			{/* <iframe
-				width='400'
-				height='300'
-				src={url}
-				frameborder='0'
-				allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-				allowfullscreen
-        title="This is a unique title"
-			></iframe> */}
+			<img className='img-video' src={cover} alt='card cover' />
 			<h3>{title}</h3>
 			<p>{description}</p>
 		</div>
