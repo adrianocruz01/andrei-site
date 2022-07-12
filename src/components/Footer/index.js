ceb494893898249e1c9550246bde67cab5ea83d8
marthony <marthony13@gmail.com>
2022-07-12 11:21:19 -0300

    Alocação dos links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,9 +6,9 @@ import "./style.css";
 export default function Footer() {
 	return (
 		<div className='co3-content'>
-			<img src={ImgYoutube} alt='youtube' className='ico' />
-			<img src={ImgInstagram} alt='Instagram' className='ico' />
-			<img src={ImgTelegram} alt='telegram' className='ico-telegram' />
+			<a href="https://www.youtube.com/channel/UCEY6Eje_I9BeXZlpcOks5MA" target="_blank"><img src={ImgYoutube} alt='youtube' className='ico' /></a>
+			<a href="https://www.instagram.com/_andreicruz/" target="_blank"><img src={ImgInstagram} alt='Instagram' className='ico' /></a>
+			<a href="https://t.me/GoldarioPortuguese" target="_blank"><img src={ImgTelegram} alt='telegram' className='ico-telegram' /></a>
 		</div>
 	);
 }
